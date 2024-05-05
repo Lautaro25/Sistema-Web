b33b7987ce5e87783a4497abee98c691aeb6b3fe
Lautaro <lautarovillabona.ar@gmail.com>
2024-05-04 23:25:46 -0300

    Anclaje al menú

---
+++ código.js
@@ -0,0 +1,10 @@
+document.getElementById("Input_Menú").addEventListener("submit", function (event) {
+    event.preventDefault(); // Evita que el formulario se envíe por defecto
+
+    // Aquí puedes realizar validaciones u otras acciones
+
+    // Luego, redirige al usuario a la página de destino
+    window.location.href = "Menú.html";
+
+
+});

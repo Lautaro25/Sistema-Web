d4483ad37cf180687b560ab7b68b3de78f144e56
Lautaro <lautarovillabona.ar@gmail.com>
2024-05-10 21:27:36 -0300

    s

--- código.js
+++ código.js
@@ -1,4 +1,12 @@
+var select = document.getElementById('deporte');
 
+// Agregar un evento change al select
+select.addEventListener('change', function () {
+    // Obtener el valor seleccionado
+    var opcionSeleccionada = select.value;
+    // Mostrar la opción seleccionada en la consola
+    console.log('Opción seleccionada:', opcionSeleccionada);
+});
 
 
 
